c4f197aaeb888885a37e930e5207b2edd1c51678
Kush2011 <74670716+Kush2011@users.noreply.github.com>
2021-03-19 15:53:35 +0530

    Add files via upload

--- kush.js
+++ kush.js
@@ -81,3 +81,91 @@ function mykeydown(e) {
         console.log("key w");
     }
 }
+
+function car1_up()
+{
+    if (car1_y > 0){
+        car1_y = car1_y - 10;
+        console.log("When up arrow key is pressed, x = " + car1_x + "y = " + car1_y);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
+
+function car1_down()
+{
+    if (car1_y < 600){
+        car1_y = car1_y + 10;
+        console.log("When up down key is pressed, x = " + car1_x + "y = " + car1_y);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
+
+function car1_left()
+{
+    if (car1_x > 0){
+        car1_x = car1_x + 10;
+        console.log("When up left key is pressed, x = " + car1_x + "y = " + car1_y);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
+
+function car1_right()
+{
+    if (car1_x < 800){
+        car1_x = car1_x - 10;
+        console.log("When up right key is pressed, x = " + car1_x + "y = " + car1_y);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
+
+function car2_up()
+{
+    if (car2_y > 0){
+        car2_y = car2_y - 10;
+        console.log("When up w key is pressed, x = " + car2_x + "y = " + car2_y);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
+
+function car2_down()
+{
+    if (car2_y < 600){
+        car2_y = car2_y + 10;
+        console.log("When up s key is pressed, x = " + car2_x + "y = " + car2_y);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
+
+function car2_left()
+{
+    if (car2_x > 0){
+        car2_x = car2_x - 10;
+        console.log("When up a key is pressed, x = " + car2_x + "y = " + car2_y);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
+
+function car2_right()
+{
+    if (car2_x < 800){
+        car2_x = car2_x + 10;
+        console.log("When up a key is pressed, x = " + car2_x + "y = " + car2_y);
+        uploadBackground();
+        uploadcar1();
+        uploadcar2();
+    }
+}
